47e88f248fd94ba1eef8a6361a96dd5f100a3a1e
Sabina B <sabina.motrescu@gmail.com>
2023-02-04 10:03:52 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
+import java.lang.constant.Constable;
+
 public class Main {
-    public static String encrypt (String weareTesting, int shiftKey) {
-        weareTesting = weareTesting.toLowerCase();
-        String cipherText = "";
-        for (int i = 0; i< weareTesting.length(); i++) {
-            int charPosition = weareTesting.charAt(i) - 'a';
-            int keyVal = (shiftKey + charPosition) % 26;
-            char replaceVal = (char) (keyVal + 'a');
-            cipherText += replaceVal;
+    public static void main(String[] args) {
+        System.out.println(encrypt("noworries", 3));
+    }
+
+
+    public static String encrypt(String plainText, int shift) {
+        StringBuilder encryptedText = new StringBuilder();
+        int shiftKey = 3;
+        for (int i = 0; i < plainText.length(); i++) {
+            char c = plainText.charAt(i);
+            if (Character.isLetter(c)) {
+                if (Character.isLowerCase(c)) {
+                    char newC = (char) ((c - 'a' + shift) % 26 + 'a');
+                    encryptedText.append(newC);
+                } else {
+                    char newC = (char) ((c - 'A' + shift) % 26 + 'A');
+                    encryptedText.append(newC);
+                }
+            } else {
+                encryptedText.append(c);
+            }
         }
-        return cipherText;
+        return encryptedText.toString();
     }
 }
+
+
+
 
